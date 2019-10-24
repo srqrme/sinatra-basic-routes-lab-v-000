4b95663c9fcc37e67f3596d63a0e3158d7742215
Sharmaine Requerme <srqrme@gmail.com>
2019-10-24 17:35:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,16 +1,5 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/name' do
-    "My name is Sharmaine Requerme"
-  end
-
-  get '/hometown' do
-    "My hometown is "
-  end
-
-  get '/favorite-song' do
-    "My favorite song is "
-  end
-
+  
 end
